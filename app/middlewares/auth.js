9874830d37479e107ebba45f014e9ad7fc96e94a
Emerson Muniz <emersonmuniz2013@gmail.com>
2025-01-13 19:41:52 -0300

    adicionado os endpoint de deletar uma cripto, atualizar uma cripto e ver todas as criptos

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -5,9 +5,7 @@ const User = require('../models/user');
 
 const withAuth = (req, res, next) => {
   const token = req.headers['x-access-token'];
-  //!!!!!!
-  console.log('Token: ' + token);
-  //!!!!!!
+  
   
   if (!token) {
     res.status(401).json({ e: 'No token provided' });
@@ -17,21 +15,9 @@ const withAuth = (req, res, next) => {
         res.status(401).json({ e: 'Failed to authenticate token' });
       } else {
         req.email = decoded.email;
-        //!!!!!!!
-        console.log('Usuario: ' + User);
-        console.log('Email: ' + decoded.email);
-        console.log('req.email: ' + req.email);
-        //!!!!!!!
         User.findOne({ email: decoded.email })
           .then((user) => {
-            //!!!!!!
-            console.log('User Completo: ' + user);
-            //!!!!!!
             req.user = user;
-            //!!!!!!
-            console.log('User: ' + req.user);
-            console.log('Email: ' + decoded.email);
-            //!!!!!!
             next();
           })
           .catch((err) => {
--- app/routes/cripto.js
+++ app/routes/cripto.js
@@ -5,7 +5,7 @@ const { getCriptos } = require('../../config/ApiCripto.js');
 const withAuth = require('../middlewares/auth');
 const mongoose = require('mongoose');
 
-
+//! Seleciona o nome das cripto disponíveis na API e mostra na tela
 router.get('/options', async (req, res) => {
   let criptos = [];
   let criptoJSON = await getCriptos();
@@ -21,7 +21,7 @@ router.get('/options', async (req, res) => {
   }
 });
 
-
+//! Seleciona a criptomoeda mandada pela interface, procura na API, salva no banco de dados
 router.post('/add', withAuth, async (req, res) => {
   let criptomoeda = req.body;
   let criptoBD;
@@ -29,7 +29,6 @@ router.post('/add', withAuth, async (req, res) => {
   const criptoFind = criptoJSON.find(
     (cripto) => cripto['name'] === criptomoeda['name'],
   );
-
   if (criptoFind) {
     criptoBD = new Cripto({
       symbol: criptoFind['symbol'],
@@ -49,7 +48,6 @@ router.post('/add', withAuth, async (req, res) => {
       remaining: criptoFind['remaining'],
       author: req.user._id,
     });
-    console.log('Author: ' + req.user);
   } else {
     res.status(500).json({ e: 'Cripto not found' });
   }
@@ -61,13 +59,11 @@ router.post('/add', withAuth, async (req, res) => {
   }
 });
 
-
+//! Seleciona uma criptomoeda no Banco de Dados e mostra na tela
 router.get('/view/:id', withAuth, async (req, res) => {
   try {
     const { id } = req.params;
-    console.log('-------ID: ' + id);
     let cripto = await Cripto.findById(id);
-    console.log('-------Cripto: ' + cripto);
     if (isOwner(req.user, cripto)) {
       res.status(200).json(cripto);
     } else {
@@ -80,11 +76,74 @@ router.get('/view/:id', withAuth, async (req, res) => {
 
 const isOwner = (user, cripto) => {
   if (JSON.stringify(user._id) == JSON.stringify(cripto.author._id)) {
-    return true;    
+    return true;
   } else {
     return false;
   }
 };
+
+//! Seleciona todas as criptomoedas de um mesmo usuário e mostra na tela.
+router.get('/all', withAuth, async (req, res) => {
+  try {
+    let criptos = await Cripto.find({ author: req.user._id });
+    res.status(200).json(criptos);
+  } catch (e) {
+    res.status(500).json({ e: 'Error in getting criptos' });
+  }
+});
+
+//! Recebe o nome da criptomoeda que vai ser atualizada, busca essa cripto na API, salva os novos dados no Banco de Dados e mostra na tela.
+router.put('/update/:id', withAuth, async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  let criptoJSON = await getCriptos();
+  console.log('criptoJSON', criptoJSON);
+  const criptoFind = criptoJSON.find((cripto) => cripto['name'] === name);
+  try {
+    if (criptoFind) {
+      let cripto = await Cripto.findById(id);
+      console.log('cripto', cripto);
+      console.log('req.user', req.user);
+      console.log('isOwner', isOwner(req.user, cripto));
+      console.log('user._id', req.user._id);
+      console.log('cripto.author._id', cripto.author._id);
+      if (isOwner(req.user, cripto)) {
+        let criptoUpdate = await Cripto.findOneAndUpdate(
+          {_id: id},
+          {
+            $set: {
+              name: criptoFind['name'],
+              symbol: criptoFind['symbol'],
+              rank: criptoFind['rank'],
+              price: criptoFind['price'],
+              market_cap: criptoFind['market_cap'],
+              total_volume_24h: criptoFind['total_volume_24h'],
+              low_24h: criptoFind['low_24h'],
+              high_24h: criptoFind['high_24h'],
+              delta_1h: parseFloat(criptoFind['delta_1h'].replace(',', '.')),
+              delta_24h: parseFloat(criptoFind['delta_24h'].replace(',', '.')),
+              delta_7d: parseFloat(criptoFind['delta_7d'].replace(',', '.')),
+              delta_30d: parseFloat(criptoFind['delta_30d'].replace(',', '.')),
+              markets: criptoFind['markets'],
+              last_update_timestamp: criptoFind['last_update_timestamp'],
+              remaining: criptoFind['remaining'],
+              author: req.user._id,
+            },
+          },
+          { upsert: true, new: true },
+        );
+        res.status(200).json(criptoUpdate);
+      } else {
+        res.status(403).json({ e: 'Permission denied' });
+      }
+    } else {
+      res.status(500).json({ e: 'Cripto not found' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Problem to update a note', details: error.message });
+  }
+});
 
 router.get('/teste', async (req, res) => {
   let criptos = [];
@@ -96,6 +155,24 @@ router.get('/teste', async (req, res) => {
     res.status(200).json(criptos);
   } catch (e) {
     res.status(500).json({ e: 'Error in getting criptos' });
+  }
+});
+
+//! Seleciona a criptomoeda mandada pela interface e deleta ela do Banco de Dados
+router.delete('/delete/:id', withAuth, async (req, res) => {
+  const { id } = req.params;
+  try {
+    let cripto = await Cripto.findById(id);
+    console.log('cripto:', cripto);
+    console.log('req.user', req.user);
+    if (isOwner(req.user, cripto)) {
+      await cripto.deleteOne();
+      res.status(204).json({message: "Cripto deleted"});
+    } else {
+      res.status(403).json({ e: 'Permission denied'});
+    }
+  } catch (e) {
+    res.status(500).json({ e: 'Error in deleting criptos', details: e.message });
   }
 });
 
